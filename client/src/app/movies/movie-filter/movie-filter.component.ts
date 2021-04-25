0afad2734d1a73e037dd0c769fa85785f99db3c8
aquafinatrippy <taneldeviis@hotmail.com>
2021-04-25 23:01:10 +0300

    Searching functionality

--- client/src/app/movies/movie-filter/movie-filter.component.ts
+++ client/src/app/movies/movie-filter/movie-filter.component.ts
@@ -8,11 +8,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class MovieFilterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
+
   form: FormGroup;
+
   genres = [
     { id: 1, name: 'Drama' },
     { id: 2, name: 'Comdey' },
   ];
+
+  movies = [
+    {
+      title: 'Spider-Man',
+      poster:
+        'https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+    {
+      title: 'Godzilla',
+      poster:
+        'https://images.unsplash.com/photo-1596029927859-420876aab22c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      title: 'king kong',
+      poster:
+        'https://images.unsplash.com/photo-1567922553372-4243efee9616?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1310&q=80',
+    },
+  ];
+
+  originalMovies = this.movies;
+
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
@@ -20,6 +43,22 @@ export class MovieFilterComponent implements OnInit {
       upcomingReleases: false,
       inTheaters: false,
     });
+
+    this.form.valueChanges.subscribe((values) => {
+      this.movies = this.originalMovies;
+      this.filterMovies(values);
+    });
   }
-  clearForm() {}
+
+  filterMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(
+        (movie) => movie.title.indexOf(values.title) !== -1
+      );
+    }
+  }
+
+  clearForm() {
+    this.form.reset();
+  }
 }
